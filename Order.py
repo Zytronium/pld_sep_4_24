f168b334fbc172a9fea02a6882d8a0b42ab81079
Zytronium <dastelljes@outlook.com>
2024-09-04 12:03:10 -0500

    fixed stuff; made input not case-sensitive; added menu command; etc.

--- Order.py
+++ Order.py
@@ -2,31 +2,25 @@
 import MenuItem
 
 class Order:
-    def __init__(self, items: list, total: float):
+    def __init__(self, items: list = []):
         self.items = items
-        self.total = total
 
-    def add_item(item: MenuItem):
+    def add_item(self, item: MenuItem):
         self.items.append(item)
 
-    def calculate_total() -> float:
-        new_total = 0
+    def remove_item(self, item: MenuItem):
+        self.items.remove(item)
+
+    def add_items(self, items: list):
+        for item in items:
+            self.items.append(item)
+
+    def remove_items(self, items: list):
+        for item in items:
+            self.items.remove(item)
+
+    def calculate_total(self) -> float:
+        total = 0
         for item in self.items:
-            new_total += item.price
-        return new_total
-
-
-        self.i1 = MenuItem("Salmon", 16.99),
-        self.i2 = MenuItem("Halibut", 14.99),
-        self.i3 = MenuItem("Tuna", 13.99),
-        self.i4 = MenuItem("Pineapple", 12.99),
-        self.i5 = MenuItem("Banana", 2.99),
-        self.i6 = MenuItem("Apple", 3.99),
-        self.i7 = MenuItem("Another Banana", 2.99),
-        self.i8 = MenuItem("Sandwich", 9.99)
-
-        self.item_list = [self.i1, self.i2, self.i3, self.i4, self.i5, self.i6, self.i7, self.i8]
-
-        def print_menu(self):
-            for item in self.item_list:
-                print(f"{item.name}: \t${item.price:.2f}")
+            total += item.price
+        return total
--- RestaurantOrderSystem.py
+++ RestaurantOrderSystem.py
@@ -1,7 +1,66 @@
 #!/bin/python3
-import Order
+from Order import Order
 from MenuItem import MenuItem
 
-print("Restaurant Menu:\n")
-MenuItem.print_menu()
-print("Please type the name of your first order.")
+def print_menu(menu: list):
+    for item in menu:
+        if isinstance(item, MenuItem):
+            print(f"{item.name}: ",end='')
+            space = 32 - len(item.name)
+            for i in range(space):
+                if (i + len(item.name)) % 2 == 0:
+                    print(".", end='')
+                else:
+                    print(" ",end='')
+            print(f"${item.price:.2f}")
+    print()
+
+def print_order(order: Order):
+    for item in order.items:
+        if isinstance(item, MenuItem):
+            print(f"{item.name}: {item.price:.2f}")
+    print()
+
+chicken = MenuItem("Chicken Nuggets", 6.99)
+salmon = MenuItem("Salmon Nuggets", 6.49)
+sandwich = MenuItem("Sandwich", 5.99)
+halibut = MenuItem("Halibut", 14.99)
+tuna = MenuItem("Tuna", 13.99)
+pineapple = MenuItem("Pineapple", 12.99)
+banana = MenuItem("Banana", 0.99)
+apple = MenuItem("Apple", 2.99)
+chips = MenuItem("Potato Chips", 2.99)
+water = MenuItem("Water", 1.49)
+soda = MenuItem("Soda", 3.99)
+nugget = MenuItem("Nugget Nuggets", 16.99)
+
+menu_items = [chicken, salmon, sandwich, halibut, tuna, pineapple, banana, apple, chips, water, soda, nugget]
+order = Order()
+
+print("Welcome to the restaurant. Here is the menu:")
+print_menu(menu_items)
+
+while True:
+    print("Please type an item name you'd like to order. If that is everything,"
+          "type 'done'. To see the menu again, type 'menu'.")
+    user_input = input().lower()
+    if user_input == "menu":
+        print_menu(menu_items)
+    elif user_input != "done":
+        item_found = False
+        for item in menu_items:
+            if item.name.lower() == user_input:
+                order.add_item(item)
+                item_found = True
+                print(f"{item.name}: {item.price:.2f}")
+                break
+        if not item_found:
+            print("Not a valid menu item.")
+    else:
+        break
+if len(order.items) == 0:
+    print("Really? You came to our restaurant and didn't order?")
+else:
+    print("You ordered:")
+    print_order(order)
+    print(f"Your total is ${order.calculate_total()}")
